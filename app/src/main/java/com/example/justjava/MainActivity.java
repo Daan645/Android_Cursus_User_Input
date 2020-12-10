9cf904e2b72ee9338fc58ab99ecc1cb0ffe9ec91
Daan645 <daan.jacobs@student.rocvf.nl>
2020-12-10 14:12:41 +0100

    just java afgemaakt

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,67 +1,139 @@
+
 /*
-        * IMPORTANT: Make sure you are using the correct package name.
-        * This example uses the package name:
-        * package com.example.android.justjava
-        * If you get an error when copying this code into Android studio, update it to match teh package name found
-        * in the project's AndroidManifest.xml file.
-        */
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ */
 
-        package com.example.justjava;
+package com.example.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 /*
-        * This app displays an order form to order coffee.
-        */
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
+    int whippedCream = 1;
+    int chocolate = 2;
+    boolean hasWhippedCream = false;
+    boolean hasChocolate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
     /*
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity - 1;
-
-        display(quantity);
+        displayQuantity(quantity);
     }
+
+    /**
+     * @return total price
+     */
+    private int calculatePrice() {
+        int prijs = 5;
+        if (hasWhippedCream) prijs += whippedCream;
+        if (hasChocolate) prijs += chocolate;
+
+
+        return quantity * prijs;
+    }
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String clientNameString) {
+
+
+        String priceMessage = "Name: " + clientNameString + "\nAdd whipped cream? " + addWhippedCream + "\nAdd Chocolate?" + addChocolate + "\nquantity " + +quantity + "\nTotal: " + "$" + price + " \nThank you!";
+        return priceMessage;
+    }
+
     /*
-            * This method is called when the order button is clicked.
-            */
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whippedCreamCheckbox);
+        hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolateCheckbox);
+        hasChocolate = chocolateCheckBox.isChecked();
+
+        EditText clientName = (EditText) findViewById(R.id.textBar);
+        String clientNameString = clientName.getText().toString();
+
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, clientNameString);
+
+        Intent intent = new Intent (Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Just Java order for " + clientNameString);
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+       
+
+
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private void calculatePrice(int quantity, int pricePerCup) {
+        int price = quantity * 5;
     }
 
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
+
 }
+
